e005eff02ceada45f20f9eebc7337222f2a5276f
yemsi <profe.sostenibilidad@gmailcom>
2022-02-14 15:36:05 -0500

    mi segunda funcion

--- js-math-utils/index.js
+++ js-math-utils/index.js
@@ -1,3 +1,7 @@
 export const sumar = (num1, num2) => 
 { console.log('la suma es: ${num1 + num2}');
 return num1 + num2;};
+
+export const restar = (num1, num2) => 
+{ console.log('la resta es: ${num1 - num2}');
+return num1 - num2;};
